8a02d02c1f601e45783f9ca4696ff786ee29c5ee
Alex <alex@example.com>
2026-06-03 03:09:38 +0300

    Fix node exporter install script: use curl instead of wget for download

--- install_node_exporter.sh
+++ install_node_exporter.sh
@@ -8,7 +8,7 @@ fi
 echo "Starting node_exporter installation..."
 
 # Download and extract
-wget -qO- https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz | tar xvz
+curl -sL https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz | tar xvz
 
 # Move binary
 $SUDO mv node_exporter-1.6.1.linux-amd64/node_exporter /usr/local/bin/
